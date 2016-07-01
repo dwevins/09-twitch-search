8769c984a9705ca8c47120299eeccf9813bf0bb2
Danny Evins <dcastkid@gmail.com>
2016-07-01 14:35:17 -0500

    addMessageItem works

--- src/exercise/add-message-item.js
+++ src/exercise/add-message-item.js
@@ -12,7 +12,6 @@
 import createMessageItem from 'exercise/create-message-item';
 
 export default function addMessageItem(parent, data) {
-  const item = createMessageItem();
-
+  const item = createMessageItem(data);
   parent.appendChild(item);
 }
